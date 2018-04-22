6ebcfeb77156b8d128697e66285e2395f6b77e55
Bradley Stuart Kirton <bradleykirton@gmail.com>
2018-04-22 07:23:56 +0200

    Added doc strings to the login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,17 +16,28 @@ export class LoginComponent implements OnInit {
 
   constructor(private userService: UserService) { }
 
+  /**
+    Subscribe to any authentication errors and build a message for the user.
+  */
   ngOnInit() {
     this.userService.authenticationError.subscribe( (response: HttpErrorResponse) => {
       this.errorMessage = "";
+
       Object.entries(response.error).forEach( ([key, value]) => {
         this.errorMessage += `${key} - ${value}`;
       })
       
     });
   }
+  
+  /**
+    Login function fired from the authentication form
 
-  login(username: string, password: string, remember: boolean) {
+    @param username  The user's username
+    @param password  The user's password
+    @param remember  A boolean which if checked will store the token in localStorage
+  */
+  login(username: string, password: string, remember: boolean): void {
   	this.userService.login(username, password, remember);
   }
 }
